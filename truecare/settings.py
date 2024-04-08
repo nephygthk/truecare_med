266115d1671bf5cff1b38c439fa7702d055b3c73
NephygTdkingz <tdkingzict@gmail.com>
2024-04-08 17:07:32 +0100

    added requirements for vercel deployment

--- truecare/settings.py
+++ truecare/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-3rl40rm@vhtql@sduin(z=2(7+nm&x)ejk)mwlq*tp-&q*82pb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -108,6 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
