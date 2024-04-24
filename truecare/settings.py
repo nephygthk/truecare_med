cb17164c759079cd2e7d42199a078a12176cebab
NephygTdkingz <tdkingzict@gmail.com>
2024-04-24 11:46:36 +0100

    added show receipt

--- truecare/settings.py
+++ truecare/settings.py
@@ -17,7 +17,7 @@ import cloudinary.api
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', ".vercel.app", ".now.sh"]
 
